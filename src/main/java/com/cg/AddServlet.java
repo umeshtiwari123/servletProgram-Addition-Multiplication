6ef89165e1792e7d893516800294c600038390c9
umeshtiwarijavadeveloper <umeshtiwarijavadeveloper@gmail.com>
2023-01-03 01:26:03 +0530

    Addition and Multiplication Task Added

---
+++ src/main/java/com/cg/AddServlet.java
@@ -0,0 +1,46 @@
+package com.cg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AddServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+	{
+		
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		
+		int k=i+j;
+		//k=k*k;
+		//PrintWriter out=res.getWriter();
+		
+		//out.println("Result is : "+k);
+		
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd=req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+	}
+	
+//	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+//	{
+//		
+//		int i=Integer.parseInt(req.getParameter("num1"));
+//		int j=Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k=i+j;
+//		k=k*k;
+//		PrintWriter out=res.getWriter();
+//		
+//		out.println("Result is : "+k);
+//	}
+}
